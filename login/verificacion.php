2a9edd1db7824774928f91f87fda067463582fa5
Juan Valentín Marcos Argandoña <jm7023333@gmail.com>
2026-03-16 19:59:18 +0100

    feat: login with database validation and feedback message

---
+++ login/verificacion.php
@@ -0,0 +1,32 @@
+<?php    
+	
+	usleep(600000);
+	require('../ficheros/conexion.php');
+    
+	// prueba directa
+	//$dni="00000000A";
+	//$lacontrasenia="1234";
+
+	// aquí habría que poner los filtros de seguridad
+	$dni=$_POST['eldni'];
+	$lacontrasenia=$_POST['tcontrasenia'];
+	
+	$consulta="SELECT * FROM usuarios WHERE dni='$dni' AND PASSWORD='$lacontrasenia'";
+	$resultado=mysqli_query($conexion,$consulta);
+	
+	$nregistros=mysqli_num_rows($resultado);
+	
+	if ($nregistros==0)
+	{
+		// el usuario se ha logueado MAL
+		echo 0;
+	}
+	else
+	{
+		//BIEN
+		echo 1;
+	}
+
+	// cerramos la conexión 
+	mysqli_close($conexion); 
+?>
