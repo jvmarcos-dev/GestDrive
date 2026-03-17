037ef6b334ddfb31a86f3c1266087d474a22ff8c
Juan Valentín Marcos Argandoña <jm7023333@gmail.com>
2026-03-17 10:31:08 +0100

    feat: add role-based redirect after login

--- login/verificacion.php
+++ login/verificacion.php
@@ -1,32 +1,28 @@
-<?php    
-	
-	usleep(600000);
-	require('../ficheros/conexion.php');
-    
-	// prueba directa
-	//$dni="00000000A";
-	//$lacontrasenia="1234";
+<?php
 
-	// aquí habría que poner los filtros de seguridad
-	$dni=$_POST['eldni'];
-	$lacontrasenia=$_POST['tcontrasenia'];
-	
-	$consulta="SELECT * FROM usuarios WHERE dni='$dni' AND PASSWORD='$lacontrasenia'";
-	$resultado=mysqli_query($conexion,$consulta);
-	
-	$nregistros=mysqli_num_rows($resultado);
-	
-	if ($nregistros==0)
-	{
-		// el usuario se ha logueado MAL
-		echo 0;
-	}
-	else
-	{
-		//BIEN
-		echo 1;
-	}
+usleep(600000);
+require('../ficheros/conexion.php');
 
-	// cerramos la conexión 
-	mysqli_close($conexion); 
-?>
+// prueba directa
+//$dni="00000000A";
+//$lacontrasenia="1234";
+
+// aquí habría que poner los filtros de seguridad
+$dni = $_POST['eldni'];
+$lacontrasenia = $_POST['tcontrasenia'];
+
+$consulta = "SELECT tipo FROM usuarios WHERE dni='$dni' AND PASSWORD='$lacontrasenia'";
+$resultado = mysqli_query($conexion, $consulta);
+
+$nregistros = mysqli_num_rows($resultado);
+
+if ($nregistros == 0) {
+	// el usuario se ha logueado MAL
+	echo 0;
+} else {
+	$fila = mysqli_fetch_assoc($resultado);
+	echo $fila['tipo'];
+}
+
+// cerramos la conexión 
+mysqli_close($conexion);
