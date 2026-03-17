0e62341844bdd0464a758600a36063d6e6ba2b42
Juan Valentín Marcos Argandoña <jm7023333@gmail.com>
2026-03-17 17:49:14 +0100

    refactor: convert login to SPA architecture with views

---
+++ ficheros/script.js
@@ -0,0 +1,64 @@
+//variables globales
+let idUsuario;
+let tipoUsuario;
+
+function inicio() {
+    $("#lacaja").load("vistas/login.html");
+}
+
+function envio_datos() {
+    //VALIDACION
+    // compruebo que las cajas de texto no estén vacías	
+    if ((document.getElementById('dni').value == "")) {
+        document.getElementById('dni').focus();
+        document.getElementById('info').innerHTML = "introduce usuario!";
+    } else if ((document.getElementById('password').value == "")) {
+        document.getElementById('password').focus();
+        document.getElementById('info').innerHTML = "introduce contraseña!";
+    } else {
+        // visualizo estrella
+        document.getElementById('estrella').style.visibility = 'visible';
+        // deshabilito botón
+        document.getElementById('boton1').disabled = true;
+        // borro mensaje etiqueta
+        document.getElementById('info').innerHTML = "";
+
+        // estos 2 valores habría que pasarlos por un filtro de seguridad.
+        var elusuario = document.getElementById('dni').value;
+        var lacontasenia = document.getElementById('password').value;
+
+        var url = "php/verificacion.php";
+        // hago la llamada AJAX
+        // utilizamos método "$.post" de jQuery
+
+        // aL ENVIAR DATOS TIENES QUE PASAR 3 COSAS -> LA url A LA QUE QUIERES PASARLO, LOS PARÁMETROS QUE QUIERES PASAR Y POR ULTIMO EL CALLBACK
+        //Un callback es una funcion que atiende la finalización de un script en la función
+        //Se devuelve cuando termina de ejecutarse el script
+        // solo se puede poner el nombre de la funcion del callback, no los parametros de entrada
+        $.post(url, {
+            eldni: elusuario.trim(),
+            tcontrasenia: lacontasenia.trim()
+        }, llegadaDatos1);
+    }
+}
+
+// callback llamada
+function llegadaDatos1(datos) {
+    // oculto estrella
+    document.getElementById('estrella').style.visibility = 'hidden';
+    // habilito botón
+    document.getElementById('boton1').disabled = false;
+
+    // trato error de validación.
+    //lo que hago es buscar lo que he mandado por php. En este caso lo estoy mandando como un json
+    // y tengo que buscar en el json el tipo de usuario. En este json el tipo aparece como
+    // usuario_tipo. El json seria algo como { "usuario_tipo": "alumno", "usuario_id": "5" }
+
+    if (datos.usuario_tipo) {
+        idUsuario = datos.usuario_id;
+        tipoUsuario = datos.usuario_tipo;
+        $("#lacaja").load("vistas/" + datos.usuario_tipo + "/dashboard.html");
+    } else {
+        document.getElementById('info').innerHTML = "<font color='red'>Login incorrecto</font>";
+    }
+}
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>GestDrive - Dev</title>
+    <!-- Archivos css -->
+    <link href="ficheros/login.css" rel="stylesheet">
+    <link href="ficheros/barra_menu.css" rel="stylesheet">
+    <link href="ficheros/formularios_login.css" rel="stylesheet">
+    <link href="ficheros/all.css" rel="stylesheet">
+    <!-- librería jQuery -->
+    <script type="text/javascript" src="ficheros/jquery.js"></script>
+    <!-- archivo script.js -->
+    <script type="text/javascript" src="ficheros/script.js"></script>
+</head>
+
+<body onload="inicio();">
+    <!-- **************************** CABECERA ************************************************ -->
+    <!-- **************************** CABECERA ************************************************ -->
+    <!-- **************************** CABECERA ************************************************ -->
+
+    <header>
+        <div class="BarraNavegar">
+            <i class="fas fa-tv fa-2x"></i>
+            <label class="L1">Proyecto 1º DAW</label>
+            <label class="L2"><b>Login</b></label>
+            <label class="L3">de MENTIRA</label>
+        </div>
+    </header>
+
+    <!-- **************************** CUERPO ************************************************ -->
+    <!-- **************************** CUERPO ************************************************ -->
+    <!-- **************************** CUERPO ************************************************ -->
+
+    <div class="contenedor1">
+        <div id="lacaja" class="contenedor2">
+
+            <!-- contenedor donde se cargan las vistas -->
+            <!-- contenedor donde se cargan las vistas -->
+            <!-- contenedor donde se cargan las vistas -->
+
+        </div>
+    </div>
+
+    <!-- **************************** FOOTER ************************************************ -->
+    <!-- **************************** FOOTER ************************************************ -->
+    <!-- **************************** FOOTER ************************************************ -->
+
+    <footer class="footer">
+        <img id="estrella" src="imagenes/estrella.gif" height="40" width="40" style="visibility:hidden;" />
+        <i class="fas fa-building fa-2x"></i>
+        <label>© 2026 IES Amparo Sanz</label>
+    </footer>
+
+</body>
+
+</html>
