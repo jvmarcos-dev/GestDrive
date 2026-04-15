a0c120dc8f67a7be2d8f298d0c6051dc3110bb94
Juan Valentín Marcos Argandoña <jm7023333@gmail.com>
2026-04-15 17:24:38 +0200

    feat: add google icons like svg

--- vistas/alumno/dashboard.php
+++ vistas/alumno/dashboard.php
@@ -43,9 +43,7 @@
     </div>
 
     <div id="practicas-restantes">
-        <span class="material-symbols-outlined">
-        search_hands_free
-        </span>
+        <svg class="icono-volante" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M324-111.5Q251-143 197-197t-85.5-127Q80-397 80-480t31.5-156Q143-709 197-763t127-85.5Q397-880 480-880t156 31.5Q709-817 763-763t85.5 127Q880-563 880-480t-31.5 156Q817-251 763-197t-127 85.5Q563-80 480-80t-156-31.5ZM440-164v-120q-60-12-102-54t-54-102H164q12 109 89.5 185T440-164Zm80 0q109-12 186.5-89.5T796-440H676q-12 60-54 102t-102 54v120ZM164-520h116l120-120h160l120 120h116q-15-121-105-200.5T480-800q-121 0-211 79.5T164-520Z"/></svg>
         <label>Saldo de prácticas</label>
         <label id="saldo_alumno"></label>
     </div>
@@ -54,9 +52,7 @@
     </div>
 
     <div id="consulta_proxima_practica">
-        <span class="material-symbols-outlined">
-        calendar_today
-        </span>
+        <svg class="icono-calendario" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M200-640h560v-80H200v80Zm0 0v-80 80Zm0 560q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v227q-19-9-39-15t-41-9v-43H200v400h252q7 22 16.5 42T491-80H200Zm378.5-18.5Q520-157 520-240t58.5-141.5Q637-440 720-440t141.5 58.5Q920-323 920-240T861.5-98.5Q803-40 720-40T578.5-98.5ZM787-145l28-28-75-75v-112h-40v128l87 87Z"/></svg>
         <label>Siguiente práctica</label>
         <label id="fecha"></label>
         <label id="profesor"></label>
