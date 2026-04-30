f7e293772a37051238c5c9d27a0f5210dcf7096b
Juan Valentín Marcos Argandoña <jm7023333@gmail.com>
2026-04-30 12:45:41 +0200

    feat: student page mobile style

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GestDrive - Dev</title>
     <!-- Archivos css -->
     <link href="ficheros/login.css" rel="stylesheet">
