38434da77cdfd164507b0d0ba50e0cfe18dcda08
Juan Valentín Marcos Argandoña <jm7023333@gmail.com>
2026-05-04 17:57:51 +0200

    feat: dashboard tablet and mobile mediaquery

--- vistas/alumno/dashboard.php
+++ vistas/alumno/dashboard.php
@@ -1,12 +1,12 @@
 <header>
-    <div id="imagen-header">
-        <img id="logo-header" src="imagenes/logo.png">
-    </div>
-
-    <div class="separador"></div>
-
-    <div id="contenedor-texto-header">
-        <label id="texto-header-alumno">Panel de Control del Alumno</label>
+    <div class="header-izq">
+        <div id="imagen-header">
+            <img id="logo-header" src="imagenes/logo.png">
+        </div>
+        <div class="separador"></div>
+        <div id="contenedor-texto-header">
+            <label id="texto-header-alumno">Panel de Control del Alumno</label>
+        </div>
     </div>
 
     <div id="menu-central">
@@ -14,26 +14,28 @@
         <a class="cambiar-ventana" id="header-reservar" onclick="cambiarVentana(this, 'reservar')">Reservar clases</a>
     </div>
 
-    <div id="datos-alumno-header" onclick="cargarDesplegableAlumno(event)" >
-        <img id="foto_alumno" src="">
-        <label id="nombre_alumno"></label>
-        <i class="fas fa-chevron-down flecha-perfil"></i>
-        <div class="desplegable-header-alumno">
-            <div class="item-desplegable" onclick="cambiarVentana(null, 'password')">
-                <i class="fas fa-cog"></i>
-                <button class="boton-desplegable">Cambiar contraseña</button>
-            </div>
-            <hr>
-            <div id="logout" class="item-desplegable" onclick="cerrarSesion()">
-                <i class="fas fa-sign-out-alt"></i>
-                <button class="boton-desplegable">Logout</button>
+    <div class="header-der">
+        <div id="datos-alumno-header" onclick="cargarDesplegableAlumno(event)">
+            <img id="foto_alumno" src="">
+            <label id="nombre_alumno"></label>
+            <i class="fas fa-chevron-down flecha-perfil"></i>
+            <div class="desplegable-header-alumno">
+                <div class="item-desplegable" onclick="cambiarVentana(null, 'password')">
+                    <i class="fas fa-cog"></i>
+                    <button class="boton-desplegable">Cambiar contraseña</button>
+                </div>
+                <hr>
+                <div id="logout" class="item-desplegable" onclick="cerrarSesion()">
+                    <i class="fas fa-sign-out-alt"></i>
+                    <button class="boton-desplegable">Logout</button>
+                </div>
             </div>
         </div>
     </div>
 </header>
 
 <div id="cargar-dashboard-alumno">
-    
+
 </div>
 
 <footer>
