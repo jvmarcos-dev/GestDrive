938825c2d05d802a9b3abb5eb26ae0966dfb9ee6
Juan Valentín Marcos Argandoña <jm7023333@gmail.com>
2026-05-21 18:49:02 +0200

    fix: some bugs fixed

--- index.php
+++ index.php
@@ -1,41 +1,22 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GestDrive - Dev</title>
-    <!-- Archivos css -->
     <link href="ficheros/login.css" rel="stylesheet">
     <link href="ficheros/alumno.css" rel="stylesheet">
     <link href="ficheros/admin.css" rel="stylesheet">
     <link href="ficheros/general.css" rel="stylesheet">
     <link href="ficheros/principal.css" rel="stylesheet">
     <link href="ficheros/all.css" rel="stylesheet">
-    <!-- librería jQuery -->
     <script type="text/javascript" src="ficheros/jquery.js"></script>
-    <!-- archivo script.js -->
     <script type="text/javascript" src="ficheros/script.js"></script>
-    <!-- iconos de google -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 </head>
-
 <body onload="inicio();">
-    <!-- **************************** CUERPO ************************************************ -->
-    <!-- **************************** CUERPO ************************************************ -->
-    <!-- **************************** CUERPO ************************************************ -->
     <div id="lacaja" class="contenedor2">
-
-        <!-- contenedor donde se cargan las vistas -->
-        <!-- contenedor donde se cargan las vistas -->
-        <!-- contenedor donde se cargan las vistas -->
-
     </div>
-
-    <!-- **************************** FOOTER ************************************************ -->
-    <!-- **************************** FOOTER ************************************************ -->
-    <!-- **************************** FOOTER ************************************************ -->
-
     <div id="notificacion_global" class="notificacion-oculta">
         <div id="notificacion_icono" class="notificacion-icono-contenedor"></div>
         <div class="notificacion-contenido">
@@ -45,9 +26,7 @@
         <button id="notificacion_cerrar" class="notificacion-cerrar" onclick="ocultarNotificacion()">
             <i class="fas fa-times"></i>
         </button>
-
         <div id="notificacion_progreso" class="notificacion-progreso"></div>
     </div>
 </body>
-
 </html>
